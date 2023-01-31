290f724cfc658b328bd5834187bdd10f93434da7
Vicken Matiossian <vicken107@yahoo.com>
2023-01-31 16:45:26 -0500

    Test cases updated

--- DutchAuction v1.0/test/BasicDutchAuction.ts
+++ DutchAuction v1.0/test/BasicDutchAuction.ts
@@ -42,6 +42,7 @@ beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
   // use first account to deploy the contract
   // auction = await new web3.eth.Contract(abi)
+
   // auction = (await new web3.eth.Contract(abi)
   //  .deploy({ data: '0x' + evm.bytecode.object, })
   //  .send({
@@ -74,56 +75,60 @@ describe('Dutch Auction Contract', () => {
 // setting up the parameters of the contract
 describe("Setting up auction", function () {
 
-  it('creating auction parameters', async function(){
+  it('seller owner of contract', async function(){
+
+  });
+
+  it('setting auction parameters', async function(){
 
   });
   
 });
 
 // testing contract functionality
-describe("testing functions", function () {
+describe("Testing functionality", function () {
 
-  it('is seller the owner of the contract', async function(){
-
-  });
-
-  it('someone has already won the auction', async function(){
+  it('seller - owner of the contract', async function(){
 
   });
 
-  it('owner bidding on own item', async function(){
+  it('seller - bidding on own item', async function(){
 
   });
 
-  it('all bidding blocks have passed, auction is over', async function(){
+  it('auction ended - winner already chosen', async function(){
 
   });
 
-  it('bid amount does not meet the current price', async function(){
+  it('auction ended - reject bid because select number of blocks passed', async function(){
 
   });
 
-  it('seller balance increases', async function(){
+  it('bid accepted - sufficient amount', async function(){
 
   });
 
-  it('buyer balance decreases', async function(){
+  it('bid rejected - insufficient amount', async function(){
 
   });
 
-  it('owner bidding on own item', async function(){
+  it('multiple bids - first greater than current price, second lower', async function(){
 
   });
 
-  it('2 bids, one is higher than current price, one is lower', async function(){
+  it('multiple bids - both higher than current price', async function(){
 
   });
 
-  it('2 bids, both higher than current price', async function(){
+  it('seller balance - increases', async function(){
 
   });
 
-  it('refunding losing bids', async function(){
+  it('buyer balance - decreases', async function(){
+
+  });
+
+  it('refund losing bids', async function(){
 
   });
 
