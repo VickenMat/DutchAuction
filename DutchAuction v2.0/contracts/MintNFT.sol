004cbde7d02f78c2ae8bc7189e2790da0e27abdf
Vicken Matiossian <vicken107@yahoo.com>
2023-02-15 19:25:11 -0500

    Minting testing working

--- DutchAuction v2.0/contracts/MintNFT.sol
+++ DutchAuction v2.0/contracts/MintNFT.sol
@@ -7,8 +7,8 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
+//Initializable,
 contract MintNFT is
-    Initializable,
     ERC721Upgradeable,
     ERC721URIStorageUpgradeable,
     OwnableUpgradeable
@@ -19,6 +19,25 @@ contract MintNFT is
 
     uint256 public maxSupply;
 
+    constructor(uint256 _maxSupply) {
+        maxSupply = _maxSupply;
+        require(maxSupply >= 1, "Max token supply must be greater than 0"); // throws error if max supply is set to 0
+        require(
+            maxSupply <= 500,
+            "Max token supply must be less than or equal to 500"
+        ); // throws error if max supply is set to a number greater than 500
+    }
+
+    // mints the nft
+    function safeMint(address to, string memory uri) public {
+        uint256 tokenId = _tokenIdCounter.current(); // sets tokenId to the current number in the counter
+        require(tokenId <= (maxSupply - 1), "Max number of tokens minted"); // checks if the number of minted nfts have surpassed the max supply we set earlier
+        _tokenIdCounter.increment(); // increments token counter for every successful nft mint
+        _safeMint(to, tokenId); // mints token to owners address and sets it to specific tokenId
+        _setTokenURI(tokenId, uri); // links tokenId with uri
+    }
+
+    /*
     // fn to initialize the nft to the parameters set below
     function initialize(uint256 _maxSupply) public initializer {
         maxSupply = _maxSupply;
@@ -31,15 +50,7 @@ contract MintNFT is
         __ERC721URIStorage_init();
         __Ownable_init();
     }
-
-    // mints the nft
-    function safeMint(address to, string memory uri) public {
-        uint256 tokenId = _tokenIdCounter.current(); // sets tokenId to the current number in the counter
-        require(tokenId <= (maxSupply - 1), "Max number of tokens minted"); // checks if the number of minted nfts have surpassed the max supply we set earlier
-        _tokenIdCounter.increment(); // increments token counter for every successful nft mint
-        _safeMint(to, tokenId); // mints token to owners address and sets it to specific tokenId
-        _setTokenURI(tokenId, uri); // links tokenId with uri
-    }
+*/
 
     // The following functions are overrides required by Solidity
 
---
+++ DutchAuction v2.0/test/Test.ts
@@ -0,0 +1,48 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { assert } from "console";
+import { MintNFT } from "../typechain-types";
+import { NFTDutchAuction } from "../typechain-types/contracts/NFTDutchAuction";
+import { any } from "hardhat/internal/core/params/argumentTypes";
+import { FactoryOptions } from "@nomiclabs/hardhat-ethers/types";
+import { Signer } from "ethers";
+
+describe("MintNFT", function () {
+  let MintNFT: any;
+  let mintNFT: any;
+  let owner: any;
+  let addr1: any;
+
+  beforeEach(async function () {
+    MintNFT = await ethers.getContractFactory("MintNFT");
+    [owner, addr1] = await ethers.getSigners();
+    mintNFT = await MintNFT.deploy(10);
+    await mintNFT.deployed();
+  });
+
+  describe("deployment", function () {
+    it("should set the max supply", async function () {
+      expect(await mintNFT.maxSupply()).to.equal(10);
+    });
+  });
+
+  describe("safeMint", function () {
+    it("should mint a new NFT to the owner", async function () {
+      const uri = "https://example.com/token/1";
+      await mintNFT.safeMint(owner.address, uri);
+      expect(await mintNFT.balanceOf(owner.address)).to.equal(1);
+    });
+
+    it("should not mint more tokens than the max supply", async function () {
+      const uri = "https://example.com/token/1";
+      for (let i = 0; i < 10; i++) {
+        await mintNFT.safeMint(owner.address, uri);
+      }
+      await expect(mintNFT.safeMint(owner.address, uri)).to.be.revertedWith(
+        "Max number of tokens minted"
+      );
+    });
+  });
+});
