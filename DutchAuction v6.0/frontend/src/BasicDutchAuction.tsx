f76f2f0fb1bf7536ec64eb9429812e402a044615
Vicken Matiossian <113546737+VickenMat@users.noreply.github.com>
2023-03-29 15:13:35 -0400

    Getting contract information to show upon connecting

--- DutchAuction v6.0/frontend/src/BasicDutchAuction.tsx
+++ DutchAuction v6.0/frontend/src/BasicDutchAuction.tsx
@@ -1,52 +1,72 @@
 import React, { useState, useEffect } from 'react';
-import { ethers, Contract, Signer } from 'ethers';
+import { ethers, Signer, Contract } from 'ethers';
 import BDA_abi from './utils/BasicDutchAuction.json';
 
-const contractAddress = '0x...'; // Replace with the deployed contract address
+const contractAddress = '...'; // Replace with the deployed contract address
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
-let address ="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-
-
+// let address = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
 
 function App() {
   // check if user is using metamask
-  if(window.ethereum){
-  
-  }
-  else{
-    alert("please install metamask extension")
-  }
-
-
+  // if(typeof window.ethereum !== 'undefined') {
+  // if(window.ethereum)
+  //   console.log('Web3 user detected');
+  // }
+  // else{
+  //   alert("please install metamask extension")
+  // }
   const [data, setdata] = useState({
     address:'',     // Stores address
     Balances: null  // Stores balance
   })
+  const [connectedWallet, setConnectedAddress] = useState('')
+  const [bal, setAddressBalance] = useState('')
+  const [contractAddress1, setContractAddress] = useState('')
 
-  async function connect() {
-
-    // if metamask is installed we request the account
-  window.ethereum.request({method: 'eth_requestAccounts'})
-  .then((res : any)=>{
-    console.log(res);
-    return contractAddress;
-  })
-
-    // getting the balance 
+  const connect =() =>  {
+    // connect wallet
+    window.ethereum.request({method: 'eth_requestAccounts'})
+    .then((result: any)=>{
+      console.log(result);
+      setConnectedAddress(result);
+      setBalance(result);
+      setAuctionContractAddress(result);
+    })
+    // function to get connected address's balance
+    async function setBalance(connectedWallet:any){
     window.ethereum.request({
       method:'eth_getBalance',
-      params:[address, 'latest']
+      params:[String(connectedWallet), 'latest']
     }).then((balance : any) => {
+      setAddressBalance(ethers.utils.formatEther(balance));
       // return string value to convert it into int balances
       console.log(balance)
       console.log(ethers.utils.formatEther(balance))
     })
     .catch((error:any)=>console.log(error));
+    }
+    // function to return contract address
+    async function setAuctionContractAddress(connectedWallet:any){
+      window.ethereum.request({
+        method:'eth_getBalance',
+        params:[String(connectedWallet), 'latest']
+      }).then((cAddress : any) => {
+        setContractAddress(cAddress);
+        // return string value to convert it into int balances
+        console.log(cAddress)
+      })
+    }
   }
 
   async function deploy(){
-
+    // const [owner, address1, address2] = await ethers.getSigners();
+    // seller1 = owner;
+    // account1 = address1;
+    // account2 = address2;
+    // const MintDutchAuctionFactory = await ethers.getContractFactory("BasicDutchAuction");
+    // const mintDutchAuction = await MintDutchAuctionFactory.deploy(100, 10, 10);
+    // basicDutchAuctionToken = await mintDutchAuction.deployed();
   }
 
   const [reservePrice, setReservePrice] = useState('');
@@ -73,10 +93,9 @@ function App() {
 
   // current version just for testing
   async function info(){
-    setReservePrice('100');
-    setNumBlocksAuctionOpen('10');
-    setOfferPriceDecrement('10');
-
+    // setReservePrice('100');
+    // setNumBlocksAuctionOpen('10');
+    // setOfferPriceDecrement('10');
   }
 
   async function bid() {
@@ -101,6 +120,9 @@ function App() {
           <button onClick={connect}>Connect</button>
           {/* <button disabled onClick={disconnect}>Disconnect</button> */}
         </h2>
+          <p>Connected Wallet Address <br></br>{connectedWallet}</p>
+          <p>Wallet Balance <br></br>{bal}</p>
+          <p>Contract Address <br></br>{contractAddress1}</p>
       </div>
       <div>
         <h2>Deployment</h2>
